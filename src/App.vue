0c1441e6b934ac34e4e8a52065c1c67af430775c
yahtzee2 <tawanchai_champ@hotmail.com>
2019-04-30 13:44:53 +0700

    edit_1

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <router-view/>
+    <!--<router-view/>-->
   </div>
 </template>
 
 <script>
-export default {
+export default {``
   name: 'App'
+  components:{
+    MyComponent,
+    HelloWorld
+  }
 }
 </script>
 
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="helloText">
+    Hello by ThitiBlog
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyComponent'
+}
+</script>
+
+<style>
+  #helloText {
+    color: #178bff;
+  }
+</style>
